879b537b85128b8f8d3260be04cc7f5433d3fedc
Isaac Parker <parrotmac@gmail.com>
2019-01-02 01:07:31 -0800

    Restructure app config (& enforce lint)

--- fe/src/components/LoginForm.tsx
+++ fe/src/components/LoginForm.tsx
@@ -1,76 +1,76 @@
 import React from 'react';
+
 import {
-    Button,
-    Icon,
-    InputGroup,
-    Intent,
-    Spinner,
-    Tooltip,
-} from "@blueprintjs/core";
+  Button,
+  Icon,
+  InputGroup,
+  Intent,
+  Spinner,
+  Tooltip,
+} from '@blueprintjs/core';
+
+import './LoginForm.css';
 
 export interface IInputGroupExampleState {
-    disabled: boolean;
-    filterValue: string;
-    large: boolean;
-    small: boolean;
-    showPassword: boolean;
-    tagValue: string;
+  disabled: boolean;
+  filterValue: string;
+  large: boolean;
+  small: boolean;
+  showPassword: boolean;
+  tagValue: string;
 }
 
 class LoginForm extends React.Component {
-    public state: IInputGroupExampleState = {
-        disabled: false,
-        filterValue: "",
-        large: false,
-        showPassword: false,
-        small: false,
-        tagValue: "",
-    };
+  public state: IInputGroupExampleState = {
+    disabled: false,
+    filterValue: '',
+    large: false,
+    showPassword: false,
+    small: false,
+    tagValue: '',
+  };
 
-    private handleLockClick = () => this.setState({ showPassword: !this.state.showPassword });
+  private handleLockClick = () => this.setState({ showPassword: !this.state.showPassword });
 
-    render(): JSX.Element {
+  public render(): JSX.Element {
 
-        const { disabled, filterValue, large, small, showPassword, tagValue } = this.state;
+    const { disabled, filterValue, showPassword } = this.state;
 
-        const maybeSpinner = filterValue ? <Spinner size={Icon.SIZE_STANDARD} /> : undefined;
+    const maybeSpinner = filterValue ? <Spinner size={Icon.SIZE_STANDARD} /> : undefined;
 
-        const lockButton = (
-            <Tooltip content={`${showPassword ? "Hide" : "Show"} Password`} disabled={disabled}>
-                <Button
-                    disabled={disabled}
-                    icon={showPassword ? "unlock" : "lock"}
-                    intent={Intent.WARNING}
-                    minimal={true}
-                    onClick={this.handleLockClick}
-                    tabIndex={-1}
-                />
-            </Tooltip>
-        );
+    const lockButton = (
+      <Tooltip content={`${showPassword ? 'Hide' : 'Show'} Password`} disabled={disabled}>
+        <Button
+          disabled={disabled}
+          icon={showPassword ? 'unlock' : 'lock'}
+          intent={Intent.WARNING}
+          minimal={true}
+          onClick={this.handleLockClick}
+          tabIndex={-1}
+        />
+      </Tooltip>
+    );
 
-        return (
-            <div>
-                <InputGroup
-                    large={true}
-                    leftIcon="user"
-                    onChange={console.log}
-                    placeholder="you@example.com"
-                    rightElement={maybeSpinner}
-                    // value={filterValue}
-                />
-                <br />
-                <InputGroup
-                    large={true}
-                    leftIcon={"key"}
-                    placeholder="**********"
-                    rightElement={lockButton}
-                    type={showPassword ? "text" : "password"}
-                />
-                <br />
-                <Button large={true} rightIcon="arrow-right" intent="success" text="Login" />
-            </div>
-        )
-    }
+    return (
+      <div className={'LoginForm'}>
+        <InputGroup
+          large={true}
+          leftIcon="user"
+          placeholder="you@example.com"
+          rightElement={maybeSpinner}
+          // value={filterValue}
+        />
+        <InputGroup
+          large={true}
+          leftIcon={'key'}
+          placeholder="**********"
+          rightElement={lockButton}
+          type={showPassword ? 'text' : 'password'}
+        />
+        <Button large={true} rightIcon="arrow-right" intent="success" text="Login" />
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
--- fe/src/pages/LoginPage.tsx
+++ fe/src/pages/LoginPage.tsx
@@ -2,23 +2,20 @@ import React from 'react';
 
 import LoginForm from '../components/LoginForm';
 
-import './LoginPage.css'
-
-export interface ILoginPageState {}
+import './LoginPage.css';
 
 class LoginPage extends React.Component {
-    public state: ILoginPageState = {};
 
-    render(): JSX.Element {
-        return (
-            <div className={'LoginPage'}>
-                <h2>The Rusted Project</h2>
-                <hr style={{ width: "50%" }} />
-                <br />
-                <LoginForm />
-            </div>
-        )
-    }
+  public render(): JSX.Element {
+    return (
+      <div className={'LoginPage'}>
+        <h1>The Rusted Project</h1>
+        <hr style={{ width: '50%' }} />
+        <p>Please sign in</p>
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
 export default LoginPage;
