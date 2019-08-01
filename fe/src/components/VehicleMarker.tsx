d96ec2c8c2b65caa8157b553bfa9903d2f75d8e0
Isaac Parker <parrotmac@gmail.com>
2019-08-01 00:49:46 -0700

    Create vehicle marker component

---
+++ fe/src/components/VehicleMarker.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+interface IVehicleMarkerProps {
+  latitude: number
+  longitude: number
+  visible: boolean
+  label: string
+}
+
+export default class VehicleMarker extends Component<IVehicleMarkerProps> {
+  public render(): JSX.Element {
+    return (
+      <div>Vehicle Marker</div>
+    );
+  }
+}
